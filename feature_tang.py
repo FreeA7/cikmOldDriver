2259f003dd99cfe59154f9624304832ce586b386
unknown <490983063@qq.com>
2017-07-11 22:26:58 +0800

    2017/7/11 晚上把.12写完了

--- feature_tang.py
+++ feature_tang.py
@@ -5,19 +5,26 @@
 @brief: basic features
 
 """
-import numpy as np
+
 import re
 import config
 import string
 
+import scipy
+import numpy as np
+
 from collections import Counter
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.manifold import TSNE
+
 from utils import ngram_utils, nlp_utils, np_utils, dist_utils
 # from utils import time_utils, logging_utils, pkl_utils
-from sklearn.preprocessing import LabelBinarizer
-
 
 """
-Basic Features
+1. Basic Features
 
 """
 
@@ -181,7 +188,7 @@ def AttrHasIndoorOutdoor(obs):
 
 
 """
-Distance Features
+2. Distance Features
 
 """
 
@@ -277,7 +284,7 @@ def CompressionDistance_Ngram(obs, target, ngram, token_pattern=" ", aggregation
 
 
 """
-Doc2Vec Features
+3. Doc2Vec Features
 
 """
 
@@ -346,7 +353,7 @@ def Doc2Vec_RMSE(obs, target, doc2vec_model, model_prefix):
 
 
 """
-First and Last Ngram Features
+4. First and Last Ngram Features
 
 """
 
@@ -397,3 +404,287 @@ def LastIntersectRatio_Ngram(obs, target, ngram, idx, str_match_threshold, token
     obs_ngrams = ngram_utils._ngrams(obs_tokens, ngram)
     target_ngrams = ngram_utils._ngrams(target_tokens, ngram)
     return np_utils._try_divide(Count_Ngram_get_match_count(obs_ngrams, target_ngrams, idx), len(target_ngrams))
+
+
+"""
+12. Vector Space Features
+
+"""
+
+# 首先把原文件中VectorSpace类中需要使用的函数拿出来定义
+
+
+def VectorSpace_init_word_bow(ngram, vocabulary=None):
+    bow = CountVectorizer(min_df=3,
+                          max_df=0.75,
+                          max_features=None,
+                          # norm="l2",
+                          strip_accents="unicode",
+                          analyzer="word",
+                          token_pattern=r"\w{1,}",
+                          ngram_range=(1, ngram),
+                          vocabulary=vocabulary)
+    return bow
+
+
+def VectorSpace_init_word_ngram_tfidf(ngram, vocabulary=None):
+    tfidf = TfidfVectorizer(min_df=3,
+                            max_df=0.75,
+                            max_features=None,
+                            norm="l2",
+                            strip_accents="unicode",
+                            analyzer="word",
+                            token_pattern=r"\w{1,}",
+                            ngram_range=(1, ngram),
+                            use_idf=1,
+                            smooth_idf=1,
+                            sublinear_tf=1,
+                            # stop_words="english",
+                            vocabulary=vocabulary)
+    return tfidf
+
+
+def VectorSpace_init_char_tfidf(include_digit=False):
+    chars = list(string.ascii_lowercase)
+    if include_digit:
+        chars += list(string.digits)
+    vocabulary = dict(zip(chars, range(len(chars))))
+    tfidf = TfidfVectorizer(strip_accents="unicode",
+                            analyzer="char",
+                            norm=None,
+                            token_pattern=r"\w{1,}",
+                            ngram_range=(1, 1),
+                            use_idf=0,
+                            vocabulary=vocabulary)
+    return tfidf
+
+
+def VectorSpace_init_char_ngram_tfidf(ngram, vocabulary=None):
+    tfidf = TfidfVectorizer(min_df=3,
+                            max_df=0.75,
+                            max_features=None,
+                            norm="l2",
+                            strip_accents="unicode",
+                            analyzer="char",
+                            token_pattern=r"\w{1,}",
+                            ngram_range=(1, ngram),
+                            use_idf=1,
+                            smooth_idf=1,
+                            sublinear_tf=1,
+                            # stop_words="english",
+                            vocabulary=vocabulary)
+    return tfidf
+
+
+def VectorSpace_get_cooc_terms(lst1, lst2, join_str):
+    out = [""] * len(lst1) * len(lst2)
+    cnt = 0
+    for item1 in lst1:
+        for item2 in lst2:
+            out[cnt] = item1 + join_str + item2
+            cnt += 1
+    res = " ".join(out)
+    return res
+
+
+def VectorSpace_CharDistribution_normalize(text):
+    # pat = re.compile("[a-z0-9]")
+    pat = re.compile("[a-z]")
+    group = pat.findall(text.lower())
+    if group is None:
+        res = " "
+    else:
+        res = "".join(group)
+        res += " "
+    return res
+
+
+def VectorSpace_CharDistribution_preprocess(corpus):
+    return [VectorSpace_CharDistribution_normalize(text) for text in corpus]
+
+
+def VectorSpace_CharDistribution_get_distribution(obs_corpus, target_corpus):
+    # obs tfidf
+    tfidf = VectorSpace_init_char_tfidf()
+    X_obs = tfidf.fit_transform(
+        VectorSpace_CharDistribution_preprocess(obs_corpus)).todense()
+    X_obs = np.asarray(X_obs)
+    # apply laplacian smoothing
+    s = 1.
+    X_obs = (X_obs + s) / (np.sum(X_obs, axis=1)[:, None] + X_obs.shape[1] * s)
+    # targetument tfidf
+    tfidf = VectorSpace_init_char_tfidf()
+    X_target = tfidf.fit_transform(
+        VectorSpace_CharDistribution_preprocess(target_corpus)).todense()
+    X_target = np.asarray(X_target)
+    X_target = (X_target + s) / (np.sum(X_target, axis=1)
+                                 [:, None] + X_target.shape[1] * s)
+    return X_obs, X_target
+
+
+# 返回值是文本单词层面ngram的LSA
+def LSA_Word_Ngram(obs_corpus, ngram, svd_dim=100, svd_n_iter=5):
+    tfidf = VectorSpace_init_word_ngram_tfidf(ngram)
+    X = tfidf.fit_transform(obs_corpus)
+    svd = TruncatedSVD(n_components=svd_dim,
+                       n_iter=svd_n_iter, random_state=config.RANDOM_SEED)
+    return svd.fit_transform(X)
+
+
+# 返回值是文本字母层面ngram的LSA
+def LSA_Char_Ngram(obs_corpus, ngram, svd_dim=100, svd_n_iter=5):
+    tfidf = VectorSpace_init_char_ngram_tfidf(ngram)
+    X = tfidf.fit_transform(obs_corpus)
+    svd = TruncatedSVD(n_components=svd_dim,
+                       n_iter=svd_n_iter, random_state=config.RANDOM_SEED)
+    return svd.fit_transform(X)
+
+
+# 返回值是标题和描述中重复文本在单词层面ngram的LSA
+def LSA_Word_Ngram_Cooc(obs_corpus, target_corpus, obs_ngram=1, target_ngram=1, svd_dim=100, svd_n_iter=5):
+    obs_ngrams = list(map(lambda x: ngram_utils._ngrams(
+        x.split(" "), obs_ngram, "_"), obs_corpus))
+    target_ngrams = list(map(lambda x: ngram_utils._ngrams(
+        x.split(" "), target_ngram, "_"), target_corpus))
+    # cooccurrence ngrams
+    cooc_terms = list(map(lambda lst1, lst2: VectorSpace_get_cooc_terms(
+        lst1, lst2, "X"), obs_ngrams, target_ngrams))
+    # tfidf
+    tfidf = VectorSpace_init_word_ngram_tfidf(ngram=1)
+    X = tfidf.fit_transform(cooc_terms)
+    # svd
+    svd = TruncatedSVD(n_components=svd_dim,
+                       n_iter=svd_n_iter, random_state=config.RANDOM_SEED)
+    return svd.fit_transform(X)
+
+
+# 返回值是标题的TFIDF和描述的TFIDF合并后在单词层面ngram的LSA
+def LSA_Word_Ngram_Pair(obs_corpus, target_corpus, ngram=2, svd_dim=100, svd_n_iter=5):
+    tfidf = VectorSpace_init_word_ngram_tfidf(ngram=ngram)
+    X_obs = tfidf.fit_transform(obs_corpus)
+    X_target = tfidf.fit_transform(target_corpus)
+    X_tfidf = scipy.sparse.hstack([X_obs, X_target]).tocsr()
+    # svd
+    svd = TruncatedSVD(n_components=svd_dim,
+                       n_iter=svd_n_iter, random_state=config.RANDOM_SEED)
+    X_svd = svd.fit_transform(X_tfidf)
+    return X_svd
+
+
+# 返回值是标题单词层面LSA和描述单词层面LSA之间的余弦相似度
+def LSA_Word_Ngram_CosineSim(obs_corpus, target_corpus, ngram=3, svd_dim=100, svd_n_iter=5):
+    # get common vocabulary
+    tfidf = VectorSpace_init_word_ngram_tfidf(ngram)
+    tfidf.fit(list(obs_corpus) + list(target_corpus))
+    vocabulary = tfidf.vocabulary_
+    # obs tfidf
+    tfidf = VectorSpace_init_word_ngram_tfidf(ngram, vocabulary)
+    X_obs = tfidf.fit_transform(obs_corpus)
+    # targetument tfidf
+    tfidf = VectorSpace_init_word_ngram_tfidf(ngram, vocabulary)
+    X_target = tfidf.fit_transform(target_corpus)
+    # svd
+    svd = TruncatedSVD(n_components=svd_dim,
+                       n_iter=svd_n_iter, random_state=config.RANDOM_SEED)
+    svd.fit(scipy.sparse.vstack((X_obs, X_target)))
+    X_obs = svd.transform(X_obs)
+    X_target = svd.transform(X_target)
+    # cosine similarity
+    sim = list(map(dist_utils._cosine_sim, X_obs, X_target))
+    sim = np.asarray(sim).squeeze()
+    return sim
+
+
+# 返回值是标题字母层面LSA和描述字母层面LSA之间的余弦相似度
+def LSA_Char_Ngram_CosineSim(obs_corpus, target_corpus, ngram=5, svd_dim=100, svd_n_iter=5):
+    # get common vocabulary
+    tfidf = VectorSpace_init_char_ngram_tfidf(ngram)
+    tfidf.fit(list(obs_corpus) + list(target_corpus))
+    vocabulary = tfidf.vocabulary_
+    # obs tfidf
+    tfidf = VectorSpace_init_char_ngram_tfidf(ngram, vocabulary)
+    X_obs = tfidf.fit_transform(obs_corpus)
+    # targetument tfidf
+    tfidf = VectorSpace_init_char_ngram_tfidf(ngram, vocabulary)
+    X_target = tfidf.fit_transform(target_corpus)
+    # svd
+    svd = TruncatedSVD(n_components=svd_dim,
+                       n_iter=svd_n_iter, random_state=config.RANDOM_SEED)
+    svd.fit(scipy.sparse.vstack((X_obs, X_target)))
+    X_obs = svd.transform(X_obs)
+    X_target = svd.transform(X_target)
+    # cosine similarity
+    sim = list(map(dist_utils._cosine_sim, X_obs, X_target))
+    sim = np.asarray(sim).squeeze()
+    return sim
+
+
+# 返回值是标题单词层面的TFIDF和描述单词层面的TFIDF之间的余弦相似度
+def TFIDF_Word_Ngram_CosineSim(obs_corpus, target_corpus, ngram=3):
+    # get common vocabulary
+    tfidf = VectorSpace_init_word_ngram_tfidf(ngram)
+    tfidf.fit(list(obs_corpus) + list(target_corpus))
+    vocabulary = tfidf.vocabulary_
+    # obs tfidf
+    tfidf = VectorSpace_init_word_ngram_tfidf(ngram, vocabulary)
+    X_obs = tfidf.fit_transform(obs_corpus)
+    # targetument tfidf
+    tfidf = VectorSpace_init_word_ngram_tfidf(ngram, vocabulary)
+    X_target = tfidf.fit_transform(target_corpus)
+    # cosine similarity
+    sim = list(map(dist_utils._cosine_sim, X_obs, X_target))
+    sim = np.asarray(sim).squeeze()
+    return sim
+
+
+# 返回值是标题字母层面的TFIDF和描述字母层面的TFIDF之间的余弦相似度
+def TFIDF_Char_Ngram_CosineSim(obs_corpus, target_corpus, ngram=5):
+    # get common vocabulary
+    tfidf = VectorSpace_init_char_ngram_tfidf(ngram)
+    tfidf.fit(list(obs_corpus) + list(target_corpus))
+    vocabulary = tfidf.vocabulary_
+    # obs tfidf
+    tfidf = VectorSpace_init_char_ngram_tfidf(ngram, vocabulary)
+    X_obs = tfidf.fit_transform(obs_corpus)
+    # targetument tfidf
+    tfidf = VectorSpace_init_char_ngram_tfidf(ngram, vocabulary)
+    X_target = tfidf.fit_transform(target_corpus)
+    # cosine similarity
+    sim = list(map(dist_utils._cosine_sim, X_obs, X_target))
+    sim = np.asarray(sim).squeeze()
+    return sim
+
+
+# TSNE_LSA_Word_Ngram_Cooc没有了，之后做到这里的时候可以搞明白然后写一个
+
+
+# 返回值是LSA_Word_Ngram_Pair()的二维TSNE
+def TSNE_LSA_Word_Ngram_Pair(obs_corpus, target_corpus, ngram=2, svd_dim=100, svd_n_iter=5):
+    X_svd = LSA_Word_Ngram_Pair()
+    X_scaled = StandardScaler().fit_transform(X_svd)
+    X_tsne = TSNE().fit_transform(X_scaled)
+    return X_tsne
+
+
+# 返回值是标题字母分布和描述字母分布之间的比值
+def CharDistribution_Ratio(obs_corpus, target_corpus, const_A=1., const_B=1.):
+    X_obs, X_target = VectorSpace_CharDistribution_get_distribution()
+    ratio = (X_obs + const_A) / (X_target + const_B)
+    return ratio
+
+
+# 返回值是标题字母分布和描述字母分布之间的余弦相似度
+def CharDistribution_CosineSim(obs_corpus, target_corpus):
+    X_obs, X_target = VectorSpace_CharDistribution_get_distribution()
+    # cosine similarity
+    sim = list(map(dist_utils._cosine_sim, X_obs, X_target))
+    sim = np.asarray(sim).squeeze()
+    return sim
+
+
+# 返回值是标题字母分布和描述字母分布之间的KL divergence
+def CharDistribution_KL(obs_corpus, target_corpus):
+    X_obs, X_target = VectorSpace_CharDistribution_get_distribution()
+    # KL
+    kl = dist_utils._KL(X_obs, X_target)
+    return kl
